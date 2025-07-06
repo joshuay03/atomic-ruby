ecbde9c4f264c4a55303b7208ece9416657851d3
Joshua Young <djry1999@gmail.com>
2025-07-06 14:49:34 +1000

    Add missing top-level require for `AtomicBoolean`

--- lib/atomic-ruby.rb
+++ lib/atomic-ruby.rb
@@ -3,6 +3,7 @@
 require_relative "atomic-ruby/version"
 require_relative "atomic-ruby/atomic_ruby"
 require_relative "atomic-ruby/atom"
+require_relative "atomic-ruby/atomic_boolean"
 require_relative "atomic-ruby/atomic_thread_pool"
 
 module AtomicRuby
