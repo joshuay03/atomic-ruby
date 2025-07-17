aef7ca87848da9997f73b8f28460e41493f06226
Joshua Young <djry1999@gmail.com>
2025-07-17 17:59:57 +1000

    Remove empty `AtomicRuby` module

--- lib/atomic-ruby.rb
+++ lib/atomic-ruby.rb
@@ -6,6 +6,3 @@ require_relative "atomic-ruby/atom"
 require_relative "atomic-ruby/atomic_boolean"
 require_relative "atomic-ruby/atomic_thread_pool"
 require_relative "atomic-ruby/atomic_count_down_latch"
-
-module AtomicRuby
-end
